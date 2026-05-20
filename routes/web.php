981f505755fcd8b033c5ba0e321015b52521a6d5
Azka Maulidina <156559528+zMa17@users.noreply.github.com>
2026-05-20 14:16:24 +0700

    Breeze Install

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\TagController;
@@ -11,4 +12,16 @@ Route::get('/', function () {
 
 Route::resource('kategori', KategoriController::class)->except(['show']);
 Route::resource('tag', TagController::class)->except(['show']);
-Route::resource('todo', TodoController::class)->except(['show']);
+Route::resource('todo', TodoController::class)->except(['show']);  
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
